88246a2333d753519c3691bcd6553c1e083c5707
tranminhtu4112 <tmtdos4112@gmail.com>
2021-07-08 21:00:31 +0700

    update migration

--- TranMinhTu_BIgSchool/Migrations/202107081353533_AddFollowingsTable1.cs
+++ TranMinhTu_BIgSchool/Migrations/202107081353533_AddFollowingsTable1.cs
@@ -3,16 +3,14 @@ namespace TranMinhTu_BIgSchool.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class AddIsCanceledColumnToCourse : DbMigration
+    public partial class AddFollowingsTable1 : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.Courses", "IsCanceled", c => c.Boolean(nullable: false));
         }
         
         public override void Down()
         {
-            DropColumn("dbo.Courses", "IsCanceled");
         }
     }
 }
